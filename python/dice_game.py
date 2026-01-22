e8c776b5d346534d91b95d6f4afa2f9e3283c81d
saaayurz <nexus.saaayurkhaparde@gmail.com>
2026-01-22 15:08:22 +0530

    Day 11: add score counter to dice game

--- python/dice_game.py
+++ python/dice_game.py
@@ -4,17 +4,19 @@ def roll_dice():
     return random.randint(1, 6)
 
 def play_game():
+    score = 0
     print("🎲 Dice Rolling Game")
-    print("Press Enter to roll the dice or type q to quit")
+    print("Press Enter to roll | type q to quit")
 
     while True:
         choice = input("Your choice: ").lower()
 
         if choice == "q":
-            print("Game ended. Thanks for playing!")
+            print("Game over! Your total rolls:", score)
             break
 
         dice_value = roll_dice()
+        score += 1
         print("You rolled:", dice_value)
 
 play_game()
